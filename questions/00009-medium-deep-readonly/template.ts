7a8bc720aec1e2ed92d9a0813d90d4c6c3e2ad33
Karol Wons <wons.karol@gmail.com>
2022-08-18 19:28:10 +0200

    answer

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,7 @@
-type DeepReadonly<T> = any
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends { [key: string]: any }
+    ? T[K] extends (...args: any) => any
+      ? T[K]
+      : DeepReadonly<T[K]>
+    : T[K]
+}
