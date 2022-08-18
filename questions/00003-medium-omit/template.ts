6801b3bbe8e55cb5f2780517da3f3cf01fba86bf
Karol Wons <wons.karol@gmail.com>
2022-08-18 18:45:59 +0200

    answers

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,3 @@
-type MyOmit<T, K> = any
+// https://mariusschulz.com/blog/the-omit-helper-type-in-typescript
+
+type MyOmit<T, K extends keyof T> = MyPick<T, MyExclude<keyof T, K>>
--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,5 @@
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  [P in MyExclude<keyof T, K>]: T[P]
+} & {
+  readonly [P in keyof MyPick<T, K>]: T[P]
+}
